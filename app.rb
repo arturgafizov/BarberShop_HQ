13f2ce160d00f5f173553a31749fef82a18ccc86
Artur Gafizov <art05xxx@rambler.ru>
2019-12-05 01:02:56 +0300

    Add Rakefile

--- app.rb
+++ app.rb
@@ -11,5 +11,5 @@ class Client < ActiveRecord::Base
 end
 
 get '/' do
-	erb :index			
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
